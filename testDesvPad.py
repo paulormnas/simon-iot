e6dd02dc09128de5c461c7fd6cde840e548e37e1
Mylena Kathleen <assismylena@gmail.com>
2020-10-06 16:54:12 -0300

    Revert "gitignore"
    This reverts commit 1ddad2f95c62ac990ce1edaa58bd59c03f18d9fc.

--- testDesvPad.py
+++
@@ -1,9 +0,0 @@
-#Cálculo do desvio padrão
-import numpy
-
-unit_of_measurement = input("Enter the unit of measurement of the data: ")
-data = [1, 1, 1, 1]
-
-standard_deviation = float(numpy.std(data, ddof = 1))
-
-print("The sample standard deviation is: %.2f " % standard_deviation,unit_of_measurement)

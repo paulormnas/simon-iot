31da0f7e928238845bbebaa4d3e369085112041e
Paulo Nascimento <paulormnas@gmail.com>
2021-01-15 11:54:02 -0300

    Atualizado o script main.py para execuçao e controle do fluxo principal do programa.

--- SENSORES/startRegistro.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd /home/pi/Documents/exercicio-python/SENSORES
-python3 registro.py
--- main.py
+++ main.py
@@ -1,52 +1,22 @@
-# coding: utf-8
+# -*- coding: utf-8 -*-
 
-leitura = [1,2,3,4,5,6,7,8,9,10]
-
-# dados = {
-#       "id": "id do dispositivo",
-#       "modelo": "modelo do dispositivo",
-#       "data_e_hora": "O que achar melhor"
-#     }
+from peripherals.Sensors import *
 
 
 def main():
-  print("Desvio padrao das leituras: ", desvpad(leitura))
+    ### Funcao principal do programa que controla todos o fluxos de execuçao ###
+    # TODO: desenvolver funcionalidade de registro de reinicializaçao
 
+    # Configura sensores
+    dht = DHT22(pino=22, quantidade_leituras=10, intervalo_medicao=300)
+    pir = PIR(pino=17, intervalo_medicao=60)
+    lista_de_sensores = [dht, pir]
 
-def media(lista):
-    soma = 0
-    n = len(lista)
-    for i in lista:
-        soma += i
-    ma = (soma/n)
-    return ma
-
-
-def var(lista):
-    med = media(lista)
-    soma = 0
-    for i in lista:
-        soma = soma + (i-med)**2
-    return soma
-
-
-def desvpad(lista):
-    n = len(lista)
-    somaquad = var(lista)
-    variancia = (somaquad/n-1)
-    desvpadA = (variancia)**(1/2)
-    return desvpadA
-
-# #  return dsev_pad
-#   pass
-#
-# def assinarRSA(dados_pra_assinar):
-#   ''' Esta função faz a assinatura dos dados utilizando o algoritmo RSA. '''
-#
-# #  return assinatura
-#   pass
-#
+    # Loop infinito para realizar mediçoes e enviar dados para o servidor
+    while (True):
+        for sensor in lista_de_sensores:
+            sensor.ler_dados()
 
 
 if __name__ == "__main__":
-  main()
+    main()

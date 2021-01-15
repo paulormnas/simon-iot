cfc31c4f73bde413b1e05392cc5dce68280302b5
Paulo Nascimento <paulormnas@gmail.com>
2021-01-14 23:14:27 -0300

    Atualizaçao do script bash para iniciaçao automatica da aplicaçao

--- startRegistro.sh
+++ startRegistro.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cd /home/pi/exercicio-python/SENSORES
-python3 registro.py
+# A variavel DIRETORIO foi criada para adequar o caminho onde o arquivo se encontra armazenado no sistema.
+DIRETORIO="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+python3 $DIRETORIO/main.py
 
 #Seguindo as instruções conforme o link: https://www.filipeflop.com/blog/inicializacao-durante-o-boot-com-systemd-na-raspberry-pi/

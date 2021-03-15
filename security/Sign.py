29c535529290ad8dfe37717688ee145bdc099794
icaroless <icaroless@hotmail.com>
2021-03-14 21:30:19 -0300

    Adicionado o arquivo de configuracao config.ini a classe Sign

--- security/Sign.py
+++ security/Sign.py
@@ -1,15 +1,20 @@
+import configparser
 from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
 from Crypto.Signature import pkcs1_15
 
 class Signature():
-    # TODO: Adicionar informaçoes de chaves publicas e privadas nas confiuraçoes
+    def __init__(self):
+        self.config = configparser.ConfigParser()
+        self.config.read('config.ini')
+        
     def sign(self, dados):
         convert = str(dados)   #Converte o dicionario em uma string para poder ser em seguida convertido em bytes.
         bite_mensage = convert.encode()   #Converte a string em bytes para poder gerar o Hash.
         h = SHA256.new(bite_mensage)   #Gera o Hash da mensagem
         print(h.hexdigest())
-        key = RSA.import_key(open('private.pem').read())
+        configWay = self.config.get('signature','private_key')
+        key = RSA.import_key(open(configWay).read())
         assinatura = pkcs1_15.new(key).sign(h)
         print(assinatura)
         return assinatura
@@ -18,12 +23,14 @@ class Signature():
         
         copia = dados   #Copia o dicionario original para fazer as operacoes de comparacao sem alterar o original
         original = dados.get("signature")   #Armazena o valor do campo signature para ser usada na comparacao de chaves
-        key = RSA.import_key(open('public.pem').read())
+        configWay = self.config.get('signature','private_key')
+        key = RSA.import_key(open(configWay).read())
         copia.pop("signature")   #Remove o campo de assinatura da copia para poder gerar um novo hash para comparacao
         convert = str(copia)   #Converte o dicionario copiado em uma string para poder ser em seguida convertido em bytes.
         bite_mensage = convert.encode()   #Converte a string em bytes para poder gerar o Hash.
         h = SHA256.new(bite_mensage)   #Gera o Hash da mesagem
-        key = RSA.import_key(open('private.pem').read())
+        configWay = self.config.get('signature','public_key')
+        key = RSA.import_key(open(configWay).read())
         assinatura = pkcs1_15.new(key).sign(h)
         assinatura = str(assinatura)
         if (assinatura == original):

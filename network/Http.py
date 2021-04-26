b9acd9bb739857e57bec93750b4e5ac8f21cedeb
Mylena Assis <assismylena@gmail.com>
2021-04-26 12:14:32 -0300

    Adicao do comando para enviar assinatura para o servidor conferi-la

--- network/Http.py
+++ network/Http.py
@@ -3,7 +3,7 @@
 import requests
 import json
 
-class NetworkManager():
+class HttpManager():
 	def __init__(self, server_url, porta):
 		self.server = server_url
 		self.port = porta
@@ -14,3 +14,11 @@ class NetworkManager():
 		url = "http://" + self.server + ":" + str(self.port) + "/registrar_dados"
 		request = requests.post(url, data=json_data)
 		print(request)
+	
+	def conferir_assinatura(self, assinatura):
+		''' Função para enviar assinatura para o servidor verifica-la via GET HTTP '''
+		json_data = json.dumps(dados)
+		url = "http://" + self.server + ":" + str(self.port) + "/verifica_dispositivo?device_id={}&data={}"
+		response = requests.get(url.format(device_id, json_data), data=assinatura)
+		print(response)
+

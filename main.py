817726bebbc2df12fba2a6c55707eb1885e26409
Paulo Nascimento <paulormnas@gmail.com>
2021-05-01 00:27:57 -0300

    Arquivos main.py e Bluetooth.py refatorados para trabalharem com modos de medidor e padrao

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 # -*- coding: utf-8 -*-
 from peripherals.Sensors import DHT22, PIR
-from network.Http import HttpManager
+from network import Bluetooth, Http
 from utils.Log import LogManager
+from utils.Config import ConfigDeviceInfo
 
 
 def main():
     log = LogManager()
     log.generate_boot_log()
-    http = HttpManager()
-    sensors_list = config_sensors()
+
+    config = ConfigDeviceInfo()
+    if config.type == 'meter':
+        run_meter_mode()
+    if config.type == 'standard':
+        run_standard_mode()
+
+
+def run_meter_mode():
+    blue = Bluetooth.BluetoothManagerMeter()
+    http = Http.HttpManager()
+    sensors = config_sensors()
     while True:
-        data_to_send = [sensor.read for sensor in sensors_list]
+        # TODO: check for calibration event
+        data_to_send = [sensor.read for sensor in sensors]
         for data in data_to_send:
             http.enviar_dados(data)
 
@@ -19,6 +31,19 @@ def config_sensors():
     dht = DHT22()
     pir = PIR()
     return [dht, pir]
+
+
+def run_standard_mode():
+    blue = Bluetooth.BluetoothManagerStandard()
+    while True:
+        handle_requests(blue)
+
+
+def handle_requests(std_bluetooth):
+    data = std_bluetooth.receive_data()
+    if data == Bluetooth.CHALLENGE:
+        challenge = std_bluetooth.receive_data()
+        std_bluetooth.sign_challenge(challenge)
 
 
 if __name__ == "__main__":
--- network/Bluetooth.py
+++ network/Bluetooth.py
@@ -3,7 +3,8 @@ import random
 import subprocess
 import bluetooth
 from utils.Log import LogManager
-from utils.Config import ConfigDeviceInfo
+from network.Http import HttpManager
+from security.Sign import Signature
 
 START_CONNECTION = '1000'
 ACK = '1001'
@@ -15,18 +16,38 @@ class BluetoothManager(object):
         self.log = LogManager()
         self.sock = None
         self.is_connected = False
-
         self.enable_device_scan()
-
-        self.config = ConfigDeviceInfo()
-        if self.config.type == 'standard':
-            self.enable_calibration_service()
-        if self.config.type == 'meter':
-            self.find_and_authenticate_standard()
 
     @staticmethod
     def enable_device_scan():
         subprocess.call(['sudo', 'hciconfig', 'hci0', 'piscan'])
+
+    def send_data(self, data):
+        # self.server_sock.settimeout(5.0)
+        self.sock.send(data)
+
+    def receive_data(self):
+        return self.sock.recv(1024)
+
+    def close_connections(self):
+        self.client_sock.close()
+        self.sock.close()
+        self.disable_device_scan()
+        self.is_connected = False
+
+    @staticmethod
+    def disable_device_scan():
+        subprocess.call(['sudo', 'hciconfig', 'hci0', 'noscan'])
+
+    @property
+    def is_connected(self):
+        return self.is_connected
+
+
+class BluetoothManagerStandard(BluetoothManager):
+    def __init__(self):
+        super().__init__()
+        self.enable_calibration_service()
 
     def enable_calibration_service(self):
         self.sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
@@ -38,6 +59,17 @@ class BluetoothManager(object):
                                     service_classes=[bluetooth.SERIAL_PORT_CLASS],
                                     profiles=[bluetooth.SERIAL_PORT_PROFILE])
         print("Aguardando conexão.")
+
+    def sign_challenge(self):
+        challenge = self.receive_data()
+        signed_challenge = Signature.sign(challenge)
+        self.send_data(signed_challenge)
+
+
+class BluetoothManagerMeter(BluetoothManager):
+    def __init__(self):
+        super().__init__()
+        self.find_and_authenticate_standard()
 
     def find_and_authenticate_standard(self):
         standard_addrs = self.find_standards()
@@ -68,55 +100,39 @@ class BluetoothManager(object):
 
     def connect_to_standard(self, standard_addrs):
         for addr in standard_addrs:
-            service = bluetooth.find_service(address=addr,
-                                             name='SiMon Meter Calibration',
-                                             uuid=bluetooth.SERIAL_PORT_CLASS)
-            if len(service) > 0:
-                service = service[0]
-                port = service["port"]
-                self.sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-                self.sock.connect((addr, port))
-                self.send_data(START_CONNECTION)
-                break
-            print(f'Serviço de calibração não encontrado para o endereço: {addr}')
+            services = bluetooth.find_service(address=addr,
+                                              name='SiMon Meter Calibration',
+                                              uuid=bluetooth.SERIAL_PORT_CLASS)
+            if len(services) > 0:
+                service = services[0]
+                self.connect_socket(service, addr)
+                self.authenticate()
+                if self.is_connected:
+                    break
+            else:
+                print(f'Serviço de calibração não encontrado para o endereço: {addr}')
+
+    def connect_socket(self, service, addr):
+        port = service["port"]
+        self.sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+        self.sock.connect((addr, port))
+        self.send_data(START_CONNECTION)
 
     def authenticate(self):
         self.send_data(CHALLENGE)
         challenge_value = random.random()
         self.send_data(challenge_value)
         data = self.receive_data()
-        # TODO: enviar data para o servidor conferir a assinatura
+        http = HttpManager()
+        response = http.conferir_assinatura(data)
+        self.check_server_response(response)
 
-        response = ''
-        self.is_standard_authenticated(response)
-
-    def send_data(self, data):
-        # self.server_sock.settimeout(5.0)
-        self.sock.send(data)
-
-    def receive_data(self):
-        return self.sock.recv(1024)
-
-    def is_standard_authenticated(self, response):
+    def check_server_response(self, response):
         if response == 'valid':
             print('Padrao autenticado pelo servidor')
-            self.log.generate_bluetooth_new_connection_log(is_valid="valid", addr=self.client_info)
+            self.log.generate_bluetooth_new_connection_log(is_valid=True, addr=self.client_info)
             self.is_connected = True
         else:
             print('Padrao nao autenticado pelo servidor')
-            self.log.generate_bluetooth_new_connection_log(is_valid="invalid", addr=self.client_info)
-            print("Closing sockets")
+            self.log.generate_bluetooth_new_connection_log(is_valid=False, addr=self.client_info)
 
-    def close_connections(self):
-        self.client_sock.close()
-        self.sock.close()
-        self.disable_device_scan()
-        self.is_connected = False
-
-    @staticmethod
-    def disable_device_scan():
-        subprocess.call(['sudo', 'hciconfig', 'hci0', 'noscan'])
-
-    @property
-    def is_connected(self):
-        return self.is_connected
